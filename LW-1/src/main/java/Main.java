765ddbb91318707c8bb0006b4e774e77b834f795
Markiyan Derevetskyi <markiyan07345@gmail.com>
2021-02-18 21:25:55 +0200

    implemented parallel algorithm

--- LW-1/src/main/java/Main.java
+++ LW-1/src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
-        
+        final int LENGTH = 1000000;
+        int []numbers = new int[LENGTH];
+        Random random = new Random();
+
+        for(int i = 0; i < LENGTH; i++) {
+            numbers[i] = random.nextInt(5000);
+        }
+
+        SearchAlgorithm searchAlgorithm = new SequentialAlgorithm(numbers);
+        long startTimeSequential = System.currentTimeMillis();
+        int resultSequential = searchAlgorithm.searchTheMostFrequentNumber();
+        long spentTimeSequential = System.currentTimeMillis() - startTimeSequential;
+        System.out.println("Sequential algorithm: result ---> " + resultSequential + ", spent time ---> " + TimeUnit.MILLISECONDS.toSeconds(spentTimeSequential) + "s.");
+
+        searchAlgorithm = new ParallelAlgorithm(numbers);
+        long startTimeParallel = System.currentTimeMillis();
+        int resultParallel = searchAlgorithm.searchTheMostFrequentNumber();
+        long spentTimeParallel = System.currentTimeMillis() - startTimeParallel;
+        System.out.println("Parallel algorithm: result ---> " + resultParallel + ", spent time ---> " + TimeUnit.MILLISECONDS.toSeconds(spentTimeParallel) + "s.");
+
     }
 }
---
+++ LW-1/src/main/java/ParallelAlgorithm.java
@@ -0,0 +1,107 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ParallelAlgorithm implements SearchAlgorithm {
+    private int[] numbers;
+
+    public ParallelAlgorithm(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public int searchTheMostFrequentNumber() {
+        final int COUNT_OF_THREADS = Runtime.getRuntime().availableProcessors();
+        final int length = numbers.length;
+        ExecutorService executorService = Executors.newFixedThreadPool(COUNT_OF_THREADS);
+        int mostFrequent = 0, countMostFrequent = 0;
+        int []alreadyChecked = new int[length];
+        int k = 0;
+
+        int block = length / COUNT_OF_THREADS;
+        int endCount = block - 1;
+
+        for (int i = 0; i < length; i++) {
+            if(i == length / 2) block--;
+            int begin = i;
+            int end = endCount;
+            int currentElement = numbers[i];
+            if(isElementAlreadyChecked(currentElement, alreadyChecked)) {
+                continue;
+            }
+            alreadyChecked[k++] = currentElement;
+            List<Future<Integer>> results = new ArrayList<>();
+
+            for (int j = 0; j < COUNT_OF_THREADS; j++) {
+                if(end >= length) {
+                    end = length - 1;
+                }
+                if(begin >= length) {
+                    begin = length - 1;
+                }
+
+                Future<Integer> result = executorService.submit(new SearchTask(numbers, currentElement, begin, end));
+                results.add(result);
+                begin = end + 1;
+                end = begin + block - 1;
+            }
+
+            int countCurrent = calculateAllEntries(results);
+            if (countCurrent > countMostFrequent) {
+                mostFrequent = currentElement;
+                countMostFrequent = countCurrent;
+            }
+            endCount++;
+        }
+
+        return mostFrequent;
+    }
+
+    private int calculateAllEntries(List<Future<Integer>> results) {
+        int count = 0;
+
+        for (Future<Integer> currentResult : results) {
+            try {
+                count += currentResult.get();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isElementAlreadyChecked(int element, int []alreadyChecked) {
+        return Arrays.stream(alreadyChecked).anyMatch(i -> i == element);
+    }
+
+    private class SearchTask implements Callable<Integer> {
+        private int[] numbers;
+        private int currentElement;
+        private int begin;
+        private int end;
+
+        public SearchTask(int[] numbers, int currentElement, int begin, int end) {
+            this.numbers = numbers;
+            this.currentElement = currentElement;
+            this.begin = begin;
+            this.end = end;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            int countCurrent = 0;
+            for (int i = begin; i <= end; i++) {
+                if (currentElement == numbers[i]) {
+                    countCurrent++;
+                }
+            }
+
+            return countCurrent;
+        }
+    }
+}

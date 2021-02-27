91d504cd2f224fc9196e267aac4770f4f799d28c
Markiyan Derevetskyi <markiyan07345@gmail.com>
2021-02-27 17:00:24 +0200

    added LW-2 module

---
+++ LW-2/src/main/java/com/derevetskyi/markiyan/lw2/Lw2Application.java
@@ -0,0 +1,13 @@
+package com.derevetskyi.markiyan.lw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lw2Application.class, args);
+    }
+
+}
---
+++ LW-2/src/test/java/com/derevetskyi/markiyan/lw2/Lw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derevetskyi.markiyan.lw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
